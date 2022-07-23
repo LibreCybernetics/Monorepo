0adc6c22d49b6ed94d8974b2be7a0d83cba574fe
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2022-07-22 22:25:57 -0500

    [util] Refactor NestedMap type with Either to simplify access

--- lib/util/src/commonMain/kotlin/util/types/NestedMap.kt
+++ lib/util/src/commonMain/kotlin/util/types/NestedMap.kt
@@ -1,36 +1,29 @@
 package util.types
 
-sealed interface NestedMap<K, V> {
-	companion object {
-		fun <K, V> empty() = NestedMapNode<K, V>(content = emptyMap())
-	}
-}
+data class NestedMap<K, V>(val content: Map<K, Either<V, NestedMap<K, V>>>) : Map<K, Either<V, NestedMap<K, V>>> {
 
-data class NestedMapLeaf<K, V>(val content: V) : NestedMap<K, V>
-data class NestedMapNode<K, V>(val content: Map<K, NestedMap<K, V>>) : NestedMap<K, V>, Map<K, V> {
+	companion object {
+		fun <K, V> empty() = NestedMap<K, V>(content = emptyMap())
+	}
+
 	override val keys: Set<K> = content.keys
 	override val size: Int = content.size
 
 	override fun isEmpty(): Boolean = content.isEmpty()
 	override fun containsKey(key: K): Boolean = content.containsKey(key)
-	override fun containsValue(value: V): Boolean = content.containsValue(NestedMapLeaf(value))
+	override fun containsValue(value: Either<V, NestedMap<K, V>>): Boolean = content.containsValue(value)
 
-	override fun get(key: K): V? = when (val value = content[key]) {
-		is NestedMapLeaf -> value.content
-		is NestedMapNode -> null
-		null -> null
-	}
+	override fun get(key: K): Either<V, NestedMap<K, V>>? = content[key]
 
-	override val values: Collection<V> = keys.mapNotNull { get(it) }
+	override val values: Collection<Either<V, NestedMap<K, V>>> = keys.mapNotNull { get(it) }
 
-	override val entries: Set<Map.Entry<K, V>> = keys.mapNotNull {
+	override val entries: Set<Map.Entry<K, Either<V, NestedMap<K, V>>>> = keys.mapNotNull {
 		when (val value = get(it)) {
 			null -> null
-			else ->
-				object : Map.Entry<K, V> {
-					override val key: K = it
-					override val value: V = value
-				}
+			else -> object : Map.Entry<K, Either<V, NestedMap<K, V>>> {
+				override val key: K = it
+				override val value: Either<V, NestedMap<K, V>> = value
+			}
 		}
 	}.toSet()
 }
