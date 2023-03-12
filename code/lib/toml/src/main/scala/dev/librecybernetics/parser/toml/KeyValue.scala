99495d03a8b12e9c9ae09a4c70e8021b3e8e8f3d
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-11 21:44:35 -0600

    [toml] Add KeyMap implementation

--- code/lib/toml/src/main/scala/dev/librecybernetics/parser/toml/KeyValue.scala
+++ code/lib/toml/src/main/scala/dev/librecybernetics/parser/toml/KeyValue.scala
@@ -1,13 +1,50 @@
 package dev.librecybernetics.parser.toml
 
+import cats.data.NonEmptyList
 import cats.implicits.*
 import cats.parse.Parser
 
 import dev.librecybernetics.parser.*
 import dev.librecybernetics.types.TOML
 
+private val simpleKey: Parser[String] =
+  bareKey.backtrack | simpleLiteral.backtrack | simpleString.backtrack
+
+private val dottedkey: Parser[NonEmptyList[String]] =
+  simpleKey
+    .repSep(2, dot.surroundedBy(spaces).backtrack)
+
+private val assignment: Parser[Unit] =
+  equal.surroundedBy(spaces)
+
 val keyValue: Parser[TOML.KeyValue] =
   (
-    (bareKey.backtrack | simpleLiteral.backtrack | simpleString.backtrack),
-    spaces.with1 *> Parser.char('=') *> spaces *> allValues
+    simpleKey,
+    assignment *> allValues
   ).mapN(TOML.KeyValue.apply)
+
+// TODO: tailrec
+def transformDottedToNestedMap(
+    key: NonEmptyList[String],
+    value: TOML
+): TOML.Map =
+  val NonEmptyList(headKey, tailKeys) = key
+  tailKeys match
+    case h :: t =>
+      val nel = NonEmptyList(h, t)
+      TOML.Map(
+        Map(headKey -> transformDottedToNestedMap(nel, value))
+      )
+    case _      =>
+      TOML.Map(
+        Map(headKey -> value)
+      )
+
+val keyMap: Parser[TOML.Map] =
+  (
+    dottedkey,
+    assignment *> allValues
+  ).mapN { transformDottedToNestedMap }
+
+val keyValueOrMap: Parser[TOML] =
+  keyValue.backtrack | keyMap
