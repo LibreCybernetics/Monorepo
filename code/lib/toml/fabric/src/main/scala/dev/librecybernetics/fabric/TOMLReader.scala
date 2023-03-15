93663096c79fee38592ea82b481a03021e43042e
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-14 21:11:51 -0600

    [toml] Fix fabric readers :S

--- code/lib/toml/fabric/src/main/scala/dev/librecybernetics/fabric/TOMLReader.scala
+++ code/lib/toml/fabric/src/main/scala/dev/librecybernetics/fabric/TOMLReader.scala
@@ -1,50 +1,19 @@
 package dev.librecybernetics.fabric
 
 import fabric.*
-import fabric.define.DefType
 import fabric.rw.*
 
 import dev.librecybernetics.types.TOML
 
-object TOMLReader:
-  private given boolean: Reader[TOML.Boolean] = Reader(
-    { case TOML.Boolean(boolean) => Bool(boolean) }
-  )
-
-  private given comment: Reader[TOML.Comment] = Reader(
-    { case TOML.Comment(string) => Str(string) }
-  )
-
-  private given string: Reader[TOML.String]   = Reader[TOML.String](
-    { case TOML.String(string) => Str(string) }
-  )
-  private given integer: Reader[TOML.Integer] = Reader[TOML.Integer](
-    { case TOML.Integer(integer) => NumInt(integer.toLong) }
-  )
-
-  private given float: Reader[TOML.Float] = Reader(
-    { case TOML.Float(double) => NumDec(BigDecimal(double)) }
-  )
-
-  private given array: Reader[TOML.Array] = Reader(
-    { case TOML.Array(arr) => Arr(arr.map(_.json).toVector) }
-  )
-
-  private given keyValue: Reader[TOML.KeyValue] = Reader(
-    { case TOML.KeyValue(k, v) => Obj(k -> v.json) }
-  )
-
-  private given map: Reader[TOML.Map] = Reader(
-    { case TOML.Map(map) => Obj(map.view.mapValues(_.json).toMap) }
-  )
-
-  given reader: Reader[TOML] = MultiReader[TOML](
-    TOMLReader.boolean.asInstanceOf,
-    TOMLReader.comment.asInstanceOf,
-    TOMLReader.string.asInstanceOf,
-    TOMLReader.integer.asInstanceOf,
-    TOMLReader.float.asInstanceOf,
-    TOMLReader.array.asInstanceOf,
-    TOMLReader.keyValue.asInstanceOf,
-    TOMLReader.map.asInstanceOf
-  )
+given reader: Reader[TOML] = Reader[TOML](
+  {
+    case TOML.Boolean(boolean) => Bool(boolean)
+    case TOML.Comment(string)  => Str(string)
+    case TOML.String(string)   => Str(string)
+    case TOML.Integer(integer) => NumInt(integer.toLong)
+    case TOML.Float(double)    => NumDec(BigDecimal(double))
+    case TOML.Array(arr)       => Arr(arr.map(_.json).toVector)
+    case TOML.KeyValue(k, v)   => Obj(k -> v.json)
+    case TOML.Map(map)         => Obj(map.view.mapValues(_.json).toMap)
+  }
+)
--- code/lib/toml/fabric/src/main/scala/dev/librecybernetics/fabric/TOMLTestReader.scala
+++ code/lib/toml/fabric/src/main/scala/dev/librecybernetics/fabric/TOMLTestReader.scala
@@ -1,59 +1,25 @@
 package dev.librecybernetics.fabric
 
-import dev.librecybernetics.types.TOML
 import fabric.*
 import fabric.rw.*
 
-object TOMLTestReader:
-  private given boolean: Reader[TOML.Boolean] = Reader(
-    { case TOML.Boolean(boolean) =>
+import dev.librecybernetics.types.TOML
+
+given testReader: Reader[TOML] = Reader[TOML](
+  {
+    case TOML.Boolean(boolean) =>
       Obj("type" -> "bool", "value" -> Str(boolean.toString))
-    }
-  )
-
-  private given comment: Reader[TOML.Comment] = Reader(
-    { case TOML.Comment(comment) =>
+    case TOML.Comment(comment) =>
       Obj("type" -> "comment", "value" -> Str(comment))
-    }
-  )
-
-  private given string: Reader[TOML.String]   = Reader[TOML.String](
-    { case TOML.String(string) =>
+    case TOML.String(string)   =>
       Obj("type" -> "string", "value" -> Str(string))
-    }
-  )
-  private given integer: Reader[TOML.Integer] = Reader[TOML.Integer](
-    { case TOML.Integer(integer) =>
+    case TOML.Integer(integer) =>
       Obj("type" -> "integer", "value" -> Str(integer.toString))
-    }
-  )
-
-  private given float: Reader[TOML.Float] = Reader(
-    { case TOML.Float(double) =>
+    case TOML.Float(double)    =>
       Obj("type" -> "float", "value" -> Str(double.toString))
-    }
-  )
 
-  private given array: Reader[TOML.Array] = Reader(
-    { case TOML.Array(arr) => Arr(arr.map(_.json).toVector) }
-  )
-
-  private given keyValue: Reader[TOML.KeyValue] = Reader(
-    { case TOML.KeyValue(k, v) => Obj(k -> v.json) }
-  )
-
-  private given map: Reader[TOML.Map] = Reader(
-    { case TOML.Map(map) => Obj(map.view.mapValues(_.json).toMap) }
-  )
-
-  given reader: Reader[TOML] = MultiReader[TOML](
-    TOMLTestReader.boolean.asInstanceOf,
-    TOMLTestReader.comment.asInstanceOf,
-    TOMLTestReader.string.asInstanceOf,
-    TOMLTestReader.integer.asInstanceOf,
-    TOMLTestReader.float.asInstanceOf,
-    TOMLTestReader.array.asInstanceOf,
-    TOMLTestReader.keyValue.asInstanceOf,
-    TOMLTestReader.map.asInstanceOf
-  )
-
+    case TOML.Array(arr)     => Arr(arr.map(_.json).toVector)
+    case TOML.KeyValue(k, v) => Obj(k -> v.json)
+    case TOML.Map(map)       => Obj(map.view.mapValues(_.json).toMap)
+  }
+)
