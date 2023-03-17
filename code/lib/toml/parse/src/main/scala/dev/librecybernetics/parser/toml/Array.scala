0e3fdc09b50e6a786d29328b63abfbd14bbc1cf8
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-16 23:00:18 -0600

    [toml] Initial toml-test run
    tests: 201 passed, 213 failed
    
    real	0m31.661s
    user	0m33.546s
    sys	0m7.110s

--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Array.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Array.scala
@@ -8,20 +8,21 @@ import dev.librecybernetics.parser.toml.base.*
 import dev.librecybernetics.types.TOML
 
 object Array:
-  val bracketStart: Parser[Unit] = bracketOpen.surroundedBy(spaces)
-  val bracketEnd: Parser[Unit] = bracketClose.surroundedBy(spaces)
+  val bracketStart: Parser[Unit]  = bracketOpen.surroundedBy(spaces)
+  val bracketEnd: Parser[Unit]    = bracketClose.surroundedBy(spaces)
+  val trailingComma: Parser[Unit] = (emptyLine.rep0.with1 *> comma <* emptyLine.rep0).backtrack
 
   lazy val scalarArray: Parser[TOML] =
     scalarValues
       .repSep(comma.surroundedBy(spaces).backtrack)
-      .between(bracketStart, bracketEnd)
+      .between(bracketStart, trailingComma.? ~ bracketEnd)
       .map(arr => TOML.Array(arr.toList))
       .withContext("scalarArray")
 
   lazy val array: Parser[TOML] = Parser.recursive[TOML] { p =>
     scalarArray.backtrack | p
       .repSep(comma.surroundedBy(spaces).backtrack)
-      .between(bracketStart, bracketClose)
+      .between(bracketStart, trailingComma.? ~ bracketEnd)
       .map { (arr: NonEmptyList[TOML]) => TOML.Array(arr.toList) }
       .withContext("recursiveArray")
   }
--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Toml.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Toml.scala
@@ -3,11 +3,13 @@ package dev.librecybernetics.parser.toml
 import cats.parse.Parser
 
 import dev.librecybernetics.parser.*
+import dev.librecybernetics.parser.toml.base.*
 import dev.librecybernetics.types.TOML
 import dev.librecybernetics.types.toml.semigroupTOMLMap
 
 object Toml:
   val toml: Parser[TOML] =
-    ArrayOfTables.arrayOfTables
-      .repSep(newline ~ emptyLine.rep0)
-      .map(nel => nel.reduce(using semigroupTOMLMap))
+    (emptyLine | (comment ~ newline)).rep0.with1 *>
+      (ArrayOfTables.arrayOfTables | table | keyValueOrMap)
+        .repSep(newline ~ (emptyLine | (comment ~ newline)).rep0)
+        .map(nel => nel.reduce(using semigroupTOMLMap))
