47e797a4f1725fecfb4bb2cf84cccd4e387bac82
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-17 15:27:31 -0600

    [toml] Move implicit conversions to core

--- code/lib/toml/parse/src/test/scala/dev/librecybernetics/parser/toml/ArraySpec.scala
+++ code/lib/toml/parse/src/test/scala/dev/librecybernetics/parser/toml/ArraySpec.scala
@@ -2,25 +2,14 @@ package dev.librecybernetics.parser.toml
 
 import scala.language.implicitConversions
 
-import cats.parse.Parser
 import cats.implicits.*
+import cats.parse.Parser
 import org.scalatest.matchers.should.Matchers.*
 import org.scalatest.wordspec.AnyWordSpec
 
 import dev.librecybernetics.parser.genericTest
 import dev.librecybernetics.types.TOML
-
-given Conversion[Int, TOML] with
-  def apply(n: Int): TOML = TOML.Integer(BigInt(n))
-
-given Conversion[Double, TOML] with
-  def apply(n: Double): TOML = TOML.Float(n)
-
-given Conversion[Boolean, TOML] with
-  def apply(n: Boolean): TOML = TOML.Boolean(n)
-
-given Conversion[Seq[TOML], TOML] with
-  def apply(n: Seq[TOML]): TOML = TOML.Array(n)
+import dev.librecybernetics.types.toml.given
 
 class ArraySpec extends AnyWordSpec {
   "Array" when {
