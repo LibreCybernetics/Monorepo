e164c90706c74627f5653cd00114d763f4eaf7a7
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-18 09:25:14 -0600

    [rfc3339] Add tests to DateTime

--- code/lib/toml/parse/src/test/scala/dev/librecybernetics/parser/rfc3339/DateTimeSpec.scala
+++ code/lib/toml/parse/src/test/scala/dev/librecybernetics/parser/rfc3339/DateTimeSpec.scala
@@ -1,5 +1,20 @@
 package dev.librecybernetics.parser.rfc3339
 
-class DateTimeSpec {
+import java.time.LocalDateTime
 
+import org.scalatest.wordspec.AnyWordSpec
+
+import dev.librecybernetics.parser.*
+
+class DateTimeSpec extends AnyWordSpec {
+  "DateTime" when {
+    "Valid" should {
+      Map(
+        "2023-03-18T12:34:56" -> LocalDateTime.of(2023, 3, 18, 12, 34, 56),
+        "1990-01-01T00:00:00" -> LocalDateTime.of(1990, 1, 1, 0, 0, 0)
+      ) foreach { (s, ldt) =>
+        s in genericSuccess(dateTime)(s, ldt)
+      }
+    }
+  }
 }
