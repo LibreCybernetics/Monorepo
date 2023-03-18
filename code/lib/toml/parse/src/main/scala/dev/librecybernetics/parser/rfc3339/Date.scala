b4c1bb85bea8bbddafad3db6f5c55e5618ae2a1b
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-18 00:56:56 -0600

    [rfc3339] Improve and test date parser

--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/rfc3339/Date.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/rfc3339/Date.scala
@@ -3,25 +3,45 @@ package dev.librecybernetics.parser.rfc3339
 import scala.language.postfixOps
 import java.time.LocalDate
 
+import cats.implicits.*
 import cats.parse.Parser
 
 import dev.librecybernetics.parser.*
 
-/** TOMLv1 specifies rfc3339 date format Section 5.6 specifies:
-  *
-  * ```
-  * full-date = date-fullyear "-" date-month "-" date-mday
-  *
-  * date-fullyear = 4DIGIT
-  * date-month = 2DIGIT ; 01-12
-  * date-mday  = 2DIGIT ; 01-28, 01-29, 01-30, 01-31 based on month/year
-  * ```
+private def leapYear(year: Int): Boolean =
+  year match
+    case _ if (year % 400 == 0) => true
+    case _ if (year % 100 == 0) => false
+    case _ if (year % 4 == 0)   => true
+
+/** full-date = date-fullyear "-" date-month "-" date-mday
   */
 val date: Parser[LocalDate] =
-  (for
-    year  <- digit.rep(4).string.map(_.toShort)
-    _     <- dash
-    month <- digit.rep(2).string.map(_.toShort).filter((1 to 12) contains)
-    _     <- dash
-    day   <- digit.rep(2).string.map(_.toShort) // TODO: Filter
-  yield LocalDate.of(year, month, day)).backtrack
+  (
+    digit.rep(4, 4).string.map(_.toInt).withContext("date-fullyear"),
+    dash,
+    intIn((1 to 12), "date-month"),
+    dash,
+    intIn((1 to 31), "date-mday")
+  ).mapN((year, _, month, _, day) => (year, month, day))
+    .flatMap { case (year, month, day) =>
+      month match
+        case 2 =>
+          day match
+            case 30 | 31               =>
+              Parser.failWith(show"$year-2-$day is not a valid gregorian calendar date")
+            case 29 if !leapYear(year) =>
+              Parser.failWith(show"$year-2-$day is not a valid gregorian calendar date on non-leap years")
+            case _                     =>
+              Parser.pure((year, 2, day))
+          end match
+
+        case 4 | 6 | 9 | 11 =>
+          day match
+            case 31 =>
+              Parser.failWith(show"$year-$month-$day is not a valid gregorian calendar date")
+            case _  => Parser.pure((year, month, day))
+
+        case _ => Parser.pure((year, month, day))
+    }
+    .map(LocalDate.of(_, _, _))
