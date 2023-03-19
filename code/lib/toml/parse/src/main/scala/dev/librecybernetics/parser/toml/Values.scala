d1e9f9571669b9239413a8fdff58dc9fbc407dbb
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-18 22:16:22 -0600

    [toml] Group numeric-starting values in Values

--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Values.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Values.scala
@@ -2,20 +2,28 @@ package dev.librecybernetics.parser.toml
 
 import cats.parse.Parser
 
+import dev.librecybernetics.parser.*
+import dev.librecybernetics.parser.rfc3339.*
 import dev.librecybernetics.parser.toml.base.*
 import dev.librecybernetics.types.TOML
-import dev.librecybernetics.parser.rfc3339.*
 
-lazy val scalarValues: Parser[TOML] =
+// These all start with digit so order is important
+private val integerStartValues: Parser[TOML] =
+  Parser.peek(digit).with1 *>
+    (
+      offsetDateTime.map(TOML.OffsetDateTime.apply).backtrack |
+        dateTime.map(TOML.LocalDateTime.apply).backtrack |
+        date.map(TOML.LocalDate.apply).backtrack |
+        time.map(TOML.LocalTime.apply).backtrack |
+        Float.float.map(TOML.Float.apply).backtrack |
+        integer.map(TOML.Integer.apply).backtrack
+    )
+
+private val scalarValues: Parser[TOML] =
   (
     Boolean.boolean.map(TOML.Boolean.apply).backtrack |
       string.map(TOML.String.apply).backtrack |
-      offsetDateTime.map(TOML.OffsetDateTime.apply).backtrack |
-      dateTime.map(TOML.LocalDateTime.apply).backtrack |
-      date.map(TOML.LocalDate.apply).backtrack |
-      time.map(TOML.LocalTime.apply).backtrack |
-      Float.float.map(TOML.Float.apply).backtrack |
-      integer.map(TOML.Integer.apply).backtrack
+      integerStartValues
   ).withContext("scala-value")
 
 lazy val allValues: Parser[TOML] =
