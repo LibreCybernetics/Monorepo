f486bb159a269e8c3432b95349cb2a8608ffa10c
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-03-20 18:32:07 -0600

    [toml] Further refactoring

--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/Base.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/Base.scala
@@ -4,24 +4,24 @@ import cats.parse.{Parser, Parser0}
 
 // Char
 
-val backslash: Parser[Unit]    = Parser.char('\\')
-val bracketOpen: Parser[Unit]  = Parser.char('[')
-val bracketClose: Parser[Unit] = Parser.char(']')
-val colon: Parser[Unit]        = Parser.char(':')
-val comma: Parser[Unit]        = Parser.char(',')
-val dash: Parser[Unit]         = Parser.char('-')
-val dot: Parser[Unit]          = Parser.char('.')
-val equal: Parser[Unit]        = Parser.char('=')
-val hash: Parser[Unit]         = Parser.char('#')
-val newline: Parser[Unit]      = Parser.char('\n')
-val space: Parser[Unit]        = Parser.char(' ')
-val underscore: Parser[Unit]   = Parser.char('_')
+private[parser] val backslash: Parser[Unit]    = Parser.char('\\')
+private[parser] val bracketOpen: Parser[Unit]  = Parser.char('[')
+private[parser] val bracketClose: Parser[Unit] = Parser.char(']')
+private[parser] val colon: Parser[Unit]        = Parser.char(':')
+private[parser] val comma: Parser[Unit]        = Parser.char(',')
+private[parser] val dash: Parser[Unit]         = Parser.char('-')
+private[parser] val dot: Parser[Unit]          = Parser.char('.')
+private[parser] val equal: Parser[Unit]        = Parser.char('=')
+private[parser] val hash: Parser[Unit]         = Parser.char('#')
+private[parser] val newline: Parser[Unit]      = Parser.char('\n')
+private[parser] val space: Parser[Unit]        = Parser.char(' ')
+private[parser] val underscore: Parser[Unit]   = Parser.char('_')
 
-val digit: Parser[Char] = Parser.charIn(latinDecimalDigits)
+private[parser] val digit: Parser[Char] = Parser.charIn(latinDecimalDigits)
 
 // Spaces / Lines
 
-val spaces: Parser0[Int]            = space.rep0.map(_.length)
-val newlineOrEnd: Parser0[Unit]     = newline | Parser.end
-val emptyLine: Parser[Unit]         = (spaces.with1 ~ newline).void
-val anyUntilNewline: Parser[String] = Parser.charsWhile(_ != '\n')
+private[parser] val spaces: Parser0[Int]            = space.rep0.map(_.length)
+private[parser] val newlineOrEnd: Parser0[Unit]     = newline | Parser.end
+private[parser] val emptyLine: Parser[Unit]         = (spaces.with1 ~ newline).void
+private[parser] val anyUntilNewline: Parser[String] = Parser.charsWhile(_ != '\n')
--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/Chars.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/Chars.scala
@@ -1,12 +1,12 @@
 package dev.librecybernetics.parser
 
-val basicLatinLetters: Set[Char] =
+private[parser] val basicLatinLetters: Set[Char] =
   ('a' to 'z').toSet ++ ('A' to 'Z').toSet
 
-val latinDecimalDigits: Set[Char] =
+private[parser] val latinDecimalDigits: Set[Char] =
   ('0' to '9').toSet
   
-val hexDigit: Set[Char] =
+private[parser] val hexDigit: Set[Char] =
   latinDecimalDigits ++
     ('a' to 'f').toSet ++
     ('A' to 'F').toSet
