e5177614ae740c8b08429276d211799451847a4f
Fabián Heredia Montiel <303897+fabianhjr@users.noreply.github.com>
2023-04-22 13:31:18 -0600

    bazel+nix: init (#48)
    * bazel+nix: init
    * bazel: initial maven_install
    * direnv: use nix-community/nix-direnv
    * bazel+nix: simplify

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "LibreCybernetics Monorepo Flake";
+
+  inputs.devshell.url    = "github:numtide/devshell";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, devshell, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+
+          overlays = [ devshell.overlays.default ];
+        };
+        jdk = pkgs.jdk11;
+      in rec {
+        config = {
+          env = [{
+            name = "JAVA_HOME";
+            value = "${jdk.home}";
+          }];
+          
+          packages = with pkgs;[
+            stdenv.cc
+            bazel_6
+            jdk
+          ];
+        };
+
+        devShell = pkgs.devshell.mkShell {
+          env = config.env;
+          packages = config.packages;
+        };
+      }
+    );
+}
