c8490d227a7865e9218cc03958e1e9a577c1134c
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-05-10 16:50:31 -0600

    [rfc3339] Split off from TOML

--- code/lib/rfc3339/src/main/scala/dev/librecybernetics/parser/rfc3339/Time.scala
+++ code/lib/rfc3339/src/main/scala/dev/librecybernetics/parser/rfc3339/Time.scala
@@ -6,7 +6,6 @@ import cats.implicits.*
 import cats.parse.Parser
 
 import dev.librecybernetics.parser.*
-import dev.librecybernetics.types.TOML
 
 /** time-hour ":" time-minute ":" time-second
   */
