78fc5531842012ecdf4a8110921f5dc75c06618b
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-05-27 01:57:40 -0600

    fabric: 1.10.7 → 1.11.1

--- code/project/Version.scala
+++ code/project/Version.scala
@@ -7,7 +7,7 @@ object Version {
   lazy val cats       = "2.9.0"
   lazy val catsEffect = "3.5.0"
   lazy val catsParse  = "0.3.9"
-  lazy val fabric     = "1.10.7"
+  lazy val fabric     = "1.11.1"
   lazy val fs2        = "3.7.0"
   lazy val mouse      = "1.2.1"
   lazy val scalacheck = "1.17.0"
