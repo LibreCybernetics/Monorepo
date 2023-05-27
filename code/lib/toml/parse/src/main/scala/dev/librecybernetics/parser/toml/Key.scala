56fc5495358924efec9a3f2875a32f1c604572b4
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-05-27 15:32:43 -0600

    [toml/parse] Add ABNF from Spec to Comment

--- code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Key.scala
+++ code/lib/toml/parse/src/main/scala/dev/librecybernetics/parser/toml/Key.scala
@@ -21,26 +21,43 @@ private def transformDottedKey(
       TOML.Map(
         Map(headKey -> transformDottedKey(nel, value))
       )
-    case _      =>
+    case Nil    =>
       TOML.Map(
         Map(headKey -> value)
       )
-  end match
 
-private val bareKey: Parser[String] =
-  val setOfChars =
+/** Bare keys
+  *
+  * ```
+  * unquoted-key = 1*( ALPHA / DIGIT / %x2D / %x5F ) ; A-Z / a-z / 0-9 / - / _
+  * ```
+  */
+private val unquotedKey: Parser[String] =
+  val setOfChars: Set[Char] =
     basicLatinLetters ++
       latinDecimalDigits ++
       Set('-', '_')
 
   Parser.charIn(setOfChars).rep.string
 
-// Non-dot separated keys
-private val simpleKey: Parser[String] =
-  (bareKey.backtrack | simpleLiteral.backtrack | simpleString.backtrack)
+/** Non-dot separated keys
+  *
+  * ```
+  * quoted-key = basic-string / literal-string
+  * simple-key = quoted-key / unquoted-key
+  * ```
+  */
+private val simpleKey: Parser[String]               =
+  (unquotedKey.backtrack | simpleLiteral.backtrack | simpleString.backtrack)
     .withContext("simple-key")
 
-// Simple or Dot separated keys
+  /** Simple or Dot separated keys
+    *
+    * ```
+    * dotted-key = simple-key 1*( dot-sep simple-key )
+    * key = simple-key / dotted-key
+    * ```
+    */
 private[toml] val key: Parser[NonEmptyList[String]] =
   simpleKey
     .repSep(dot.surroundedBy(spaces).backtrack)
