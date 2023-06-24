4172cd880148b41a586bdeddaae7e105d7f6a0c9
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2023-06-23 18:49:32 -0600

    [fugitiva] Refactor CooperativeInfo to Controller

--- code/app/fugitiva/src/main/scala/coop/fugitiva/server/RootRoutes.scala
+++ code/app/fugitiva/src/main/scala/coop/fugitiva/server/RootRoutes.scala
@@ -9,6 +9,7 @@ import org.http4s.implicits.*
 import org.http4s.scalatags.scalatagsEncoder
 import scalatags.Text.all.*
 import coop.fugitiva.components.*
+import coop.fugitiva.components.controllers.CooperativeInfo
 import coop.fugitiva.components.repository.CooperativeRepository
 import coop.fugitiva.components.views.{CooperativeView, CooperativesView, IndexView}
 import coop.fugitiva.domain.*
@@ -25,32 +26,4 @@ val indexRoute = HttpRoutes.of[IO] { case GET -> Root =>
   )
 }
 
-val cooperativeRoute = HttpRoutes.of[IO] {
-  case GET -> Root / "cooperativas"      =>
-    for cooperatives <- CooperativeRepository.PostgresJAsync[IO]().getCooperatives
-    yield Response(
-      body = scalatagsEncoder.toEntity(CooperativesView(cooperatives.toSet)).body,
-      headers = Headers(
-        // TODO: add automagically?
-        `Content-Type`(MediaType.text.html, Charset.`UTF-8`)
-      )
-    )
-  case GET -> Root / "cooperativa" / url =>
-    for cooperative <- CooperativeRepository.PostgresJAsync[IO]().getCooperative(url)
-    yield cooperative match
-      case Right(cooperative) =>
-        Response(
-          body = scalatagsEncoder.toEntity(CooperativeView(cooperative)).body,
-          headers = Headers(
-            // TODO: add automagically?
-            `Content-Type`(MediaType.text.html, Charset.`UTF-8`)
-          )
-        )
-      case Left(err)          =>
-        Response(
-          status = NotFound,
-          body = scalatagsEncoder.toEntity(html(body(err.toString))).body
-        )
-}
-
-val rootRouter = (indexRoute <+> cooperativeRoute).orNotFound
+val rootRouter = (indexRoute <+> CooperativeInfo().cooperativeRoute).orNotFound
