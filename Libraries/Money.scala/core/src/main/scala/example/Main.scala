5c6623605d1101b63442e28cbfa5bb886bfa306c
Fabián Heredia Montiel <fabianhjr@protonmail.com>
2025-04-26 23:52:38 -0600

    Experiments

--- Libraries/Money.scala/core/src/main/scala/example/Main.scala
+++ Libraries/Money.scala/core/src/main/scala/example/Main.scala
@@ -1,29 +1,42 @@
 package example
 
-import scala.compiletime.testing.typeCheckErrors
+import scala.util.NotGiven
 
 type ISOCurrencyCodes = "MXN" | "USD"
 type CurrencyCodes = ISOCurrencyCodes
 
-case class Money[CurrencyCode <: CurrencyCodes](
+case class Money[ICurrencyCode <: CurrencyCodes](
     amount: BigDecimal,
-    currency: CurrencyCode
+    currency: ICurrencyCode
 ) {
+  type CurrencyCode = ICurrencyCode
+
+  def +[OCurrencyCode <: CurrencyCodes](
+                                         other: Money[OCurrencyCode]
+                                       )(using ev: CurrencyCode =:= OCurrencyCode): Money[CurrencyCode] = {
+      Money(amount + other.amount, currency)
+  }
+
   def +[OCurrencyCode <: CurrencyCodes](
       other: Money[OCurrencyCode]
-  )(using ev: CurrencyCode =:= OCurrencyCode) =
-    Money(amount + other.amount, currency)
+  )(using ev: NotGiven[CurrencyCode =:= OCurrencyCode]): Either[String, Money[CurrencyCode]]= {
+        if(currency == other.currency)
+          Right(Money(amount + other.amount, currency))
+        else
+          Left("Currencies don't match")
+  }
 }
 
 @main
 def main(args: String*): Unit = {
-  val input = Console.in.readLine()
-  input match {
-    case currencyCode: CurrencyCodes =>
-      val a = Money(10, currencyCode)
-      val b = Money(20, currencyCode)
+  val input1 = Console.in.readLine()
+  val input2 = Console.in.readLine()
+  (input1, input2) match {
+    case (currencyCode1: CurrencyCodes, currencyCode2: CurrencyCodes) =>
+      val a = Money(10, currencyCode1)
+      val b = Money(20, currencyCode2)
       val result = a + b
+      val r = Money(10, "MXN") + Money(20, "MXN")
       println(s"Result: $result")
-    case _ => println("Invalid input")
   }
 }
